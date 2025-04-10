7412b9e07bad690a140b6d797a99ceeee62664cb
vodaiphuoc <phuocvodn98gmail.com>
2025-04-10 07:10:25 +0100

    test prompt

--- main.py
+++ main.py
@@ -2,27 +2,27 @@ from llama_cpp import Llama
 import os
 import base64
 
+
+prompt = "Example of linear regression in python"
+
+
 llm = Llama(
     model_path=os.path.join(os.path.dirname(__file__), ".checkpoints","gemma-3-4b-it-q4_0.gguf"),
     n_threads=1,
     n_batch=2,
-    n_ctx=4096, # Context window,
+    n_ctx=4096,
 )
 
-prompt = "Example of linear regression in python"
-prompt_template=f'''Below is an instruction that describes a task. Write a response that appropriately completes the request.
-
-### Instruction:
-{prompt}
-
-### Response:'''
-
+prompt_template="""
+<start_of_turn>user
+{user_prompt}<end_of_turn>
+<start_of_turn>model
+"""
 
 def image_to_base64_data_uri(file_path: str):
     with open(file_path, "rb") as img_file:
         base64_data = base64.b64encode(img_file.read()).decode('utf-8')
         return f"data:image/png;base64,{base64_data}"
-
 
 response = llm.create_chat_completion(
     messages = [
@@ -30,7 +30,7 @@ response = llm.create_chat_completion(
         {
             "role": "user",
             "content": [
-                {"type" : "text", "text": "What's in this image?"},
+                {"type" : "text", "text": prompt_template.format(user_prompt = prompt)},
                 {"type": "image_url",
                  "image_url": {
                     "url": image_to_base64_data_uri("testimg.jpg") 
@@ -43,8 +43,8 @@ response = llm.create_chat_completion(
         "type": "json_object",
         "schema": {
             "type": "object",
-            "properties": {"team_name": {"type": "string"}},
-            "required": ["team_name"],
+            "properties": {"object_name": {"type": "string"}},
+            "required": ["object_name"],
         },
     },
     temperature=0.7,
