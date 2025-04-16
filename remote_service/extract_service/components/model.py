04454d02e3a83d8f5619d1adbc298938fbb38c9b
vodaiphuoc <phuocvodn98gmail.com>
2025-04-16 11:19:14 +0100

    debugging

--- remote_service/extract_service/components/model.py
+++ remote_service/extract_service/components/model.py
@@ -55,7 +55,7 @@ class UnslothExtractModel(_ExtractBase):
         from unsloth import FastLanguageModel
         from unsloth.chat_templates import get_chat_template
 
-        self.model, tokenizer = FastLanguageModel.from_pretrained(
+        self.model, preprocessor = FastLanguageModel.from_pretrained(
             model_name = "unsloth/gemma-3-4b-pt-unsloth-bnb-4bit",
             max_seq_length = self.max_out_length,
             dtype = None,
@@ -63,17 +63,17 @@ class UnslothExtractModel(_ExtractBase):
         )
         FastLanguageModel.for_inference(self.model)
 
-        self.tokenizer = get_chat_template(
-            tokenizer,
+        self.preprocessor = get_chat_template(
+            preprocessor,
             chat_template = "gemma-3"
         )
 
-        print('check type tokenizer: ', type(self.tokenizer))
+        print('check type tokenizer: ', type(self.preprocessor))
 
     def _impl_forward(self,input_prompt:str, img_paths: List[str])->str:
         content_parts = [{
                 "type": "image",
-                "image": Image.open(_img_path).convert("RGB")
+                # "image": Image.open(_img_path).convert("RGB")
             }
             for _img_path in img_paths
         ]
@@ -92,15 +92,21 @@ class UnslothExtractModel(_ExtractBase):
             }
         ]
 
-        inputs = self.tokenizer.apply_chat_template(
+        texts = self.preprocessor.apply_chat_template(
             messages,
             add_generation_prompt = True,
-            tokenize=True,
-            return_dict=True,
-            return_tensors = "pt"
+            # tokenize=True,
+            # return_dict=True,
+            # return_tensors = "pt"
+        )
+        
+        inputs = self.preprocessor(
+            [Image.open(_img_path).convert("RGB") for _img_path in img_paths], 
+            texts,
+            add_special_tokens = False,
+            return_tensors = "pt",
         ).to('cuda')
         
-
         output_tokens =  self.model.generate(
             **inputs,
             max_new_tokens = self.max_out_length-2000,
